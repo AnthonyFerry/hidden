5f79814775faf68700e9fee1a12eb48b9c4e2407
AnthonyFerry <a.ferry@ludus-academie.fr>
2017-03-16 12:39:36 +0100

    Ajout camera de base + feature de grimpette + corrections de script

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    [Header("Parametres personnage")]
     public int Life = 10;
     public float XP = 0;
+    public PlayerState State = PlayerState.standing;
 
+    [Header("Parametres vitesse")]
     public float WalkSpeed = 6.0F;
     public float JumpSpeed = 8.0F;
+    public float ClimbingSpeedDivider = 0.5F;
+    public float CrouchingSpeedDivider = 0.5F;
     public float Gravity = 20.0F;
     public int MaximumJumpCount = 1;
 
-    public float minimumY = 0f;
-    public float maximumY = 0f;
+    [Header("Camera")]
+    public GameObject Camera;
     
     private Vector3 _moveDirection = Vector3.zero;
     private float _verticalSpeed = 0;
@@ -29,32 +34,35 @@ public class Player : MonoBehaviour {
 
     void FixedUpdate()
     {
-        _verticalSpeed -= Gravity * Time.deltaTime;
+        if (State != PlayerState.climbing)
+            _verticalSpeed -= Gravity * Time.deltaTime;
+        else
+            _verticalSpeed -= 0;
     }
 
     void Update()
     {
         CharacterController controller = GetComponent<CharacterController>();
 
-        
-
         if (controller.isGrounded)
         {
+            if (State == PlayerState.climbing)
+                State = PlayerState.standing;
+
             _jumpCount = 0;
             _verticalSpeed = 0;
         }
 
-        _moveDirection = new Vector3(Input.GetAxis("horizontal"), 0, Input.GetAxis("vertical"));
-        _moveDirection = transform.TransformDirection(_moveDirection);
-        _moveDirection.x *= WalkSpeed;
-        _moveDirection.z *= WalkSpeed;
+        MovePlayer();
 
         if (Input.GetButtonDown("jump"))
+        {
             if (_jumpCount < MaximumJumpCount)
             {
                 _verticalSpeed = JumpSpeed;
                 _jumpCount++;
             }
+        }
 
         if (Input.GetButton("jump") && controller.velocity.y < -0.1 && _jumpCount > 0)
         {
@@ -63,12 +71,75 @@ public class Player : MonoBehaviour {
 
         _moveDirection.y += _verticalSpeed;
 
-
-
         controller.Move(_moveDirection * Time.deltaTime);
     }
 
-    void LateUpdate()
+    void MovePlayer()
     {
+        var horizontal = Input.GetAxis("horizontal");
+        var vertical = Input.GetAxis("vertical");
+
+        if (State == PlayerState.standing)
+        {
+            _moveDirection = new Vector3(horizontal, 0, vertical);
+            _moveDirection = transform.TransformDirection(_moveDirection);
+            _moveDirection *= WalkSpeed;
+
+            if (_moveDirection.magnitude != 0)
+                CalibrateToCameraRotation();
+        }
+        else if (State == PlayerState.climbing)
+        { 
+            _moveDirection = new Vector3(horizontal, vertical, 0);
+            _moveDirection = transform.TransformDirection(_moveDirection);
+            _moveDirection *= WalkSpeed * ClimbingSpeedDivider;
+        }
+        else if (State == PlayerState.crouching)
+        {
+            _moveDirection = new Vector3(horizontal, 0, vertical);
+            _moveDirection = transform.TransformDirection(_moveDirection);
+            _moveDirection *= WalkSpeed * CrouchingSpeedDivider;
+
+            CalibrateToCameraRotation();
+        }
     }
+
+    void CalibrateToCameraRotation()
+    {
+        if (Camera == null)
+            return;
+
+        transform.rotation = Quaternion.Euler(0, Camera.transform.localEulerAngles.y, 0);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "ClimbWall")
+        {
+            _verticalSpeed = 0;
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "ClimbWall")
+        {
+            State = PlayerState.climbing;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "ClimbWall")
+        {
+            State = PlayerState.standing;
+        }
+    }
+}
+
+public enum PlayerState
+{
+    standing,
+    climbing,
+    crouching
 }
--- Assets/scripts/ThirdPersonCamera.cs
+++ Assets/scripts/ThirdPersonCamera.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour {
 
-    public GameObject Target;
+    public Transform LookAt;
+    public Transform CameraTransform;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.position += new Vector3(Input.GetAxis("cameraX"), 0, Input.GetAxis("cameraY"));
-	}
+    private Camera _camera;
+
+    private const float WALKING_DISTANCE = 1.0f;
+    private const float PLATFORM_DISTANCE = 1.0f;
+    private const float AIMING_DISTANCE = 1.0f;
+    private const float FIGHTING_DISTANCE = 1.0f;
+
+    private float _distance = 10.0f;
+    private float _currentX = 0.0f;
+    private float _currentY = 0.0f;
+    private float _sensivityX = 4.0f;
+    private float _sensivityY = 1.0f;
+
+    private float _minY = -20f, _maxY = 50f;
+
+    private void Start()
+    {
+        CameraTransform = transform;
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        _currentX += Input.GetAxis("cameraX");
+        _currentY += Input.GetAxis("cameraY");
+
+        _currentY = Mathf.Clamp(_currentY, _minY, _maxY);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -_distance);
+        Quaternion rotation = Quaternion.Euler(_currentY, _currentX, 0);
+        CameraTransform.position = LookAt.position + rotation * dir;
+        CameraTransform.LookAt(LookAt.position);
+    }
 }
+
+public enum CameraStyle
+{
+    walking,
+    fighting,
+    platform,
+    aiming
+};
