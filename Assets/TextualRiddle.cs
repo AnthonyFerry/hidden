fe26f35170204edd70e6e97b31134941b30260df
COMPLAINTE\Elsa <e.becker@ludus-academie.com>
2017-04-04 00:55:51 +0200

    Enigme textuelle_WIP

---
+++ Assets/TextualRiddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextualRiddle : MonoBehaviour {
+    //Variables (publique commence par majuscule, privée commence par underscore)
+    public string Answer;
+    public InputField UserAnswer;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnResolve()
+    {
+        Debug.Log(UserAnswer.text);
+    }
+}
